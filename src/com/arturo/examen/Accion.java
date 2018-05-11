6a22f3886061c0618cda1e0ffc33239e69fa152b
aralvarellosblas <aralvarellosblas@victoria21.danielcastelao.org>
2018-05-11 09:52:10 +0200

    Pluggin Completado

--- src/com/arturo/examen/Accion.java
+++ src/com/arturo/examen/Accion.java
@@ -7,9 +7,14 @@ package com.arturo.examen;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(
@@ -26,6 +31,37 @@ public final class Accion implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO implement action body
+        String so=JOptionPane.showInputDialog(null, "Introduce exe para Windows y deb para Linux");
+        String cat=JOptionPane.showInputDialog(null, "Introduce el nombre de la categoria deseada");
+        String carp=JOptionPane.showInputDialog(null, "Introduce Ruta de carpeta de destino");
+        String file=JOptionPane.showInputDialog(null, "Introduce nombre deseado del Instalador");
+        String rDist=JOptionPane.showInputDialog(null, "Introduce Ruta de la carpeta dist");
+        String jar=JOptionPane.showInputDialog(null, "Introduce Nombre del archivo .jar");
+        String main=JOptionPane.showInputDialog(null, "Introduce Nombre de la clase main del programa");
+        String nApp=JOptionPane.showInputDialog(null, "Introduce Nombre de la App");
+        String title=JOptionPane.showInputDialog(null, "Introduce Titulo para la App");
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec("javapackager "
+                    + "-deploy "
+                    + "-native "+so+" -Bcategory="+cat+" "
+                    + "-outdir "+carp+" "// /home/local/DANIELCASTELAO/aralvarellosblas/Documentos/contornos/Hola/Hola "
+                    + "-outfile "+file+" "
+                    + "-srcdir "+rDist+" "// /home/local/DANIELCASTELAO/aralvarellosblas/Documentos/contornos/Hola/dist "
+                    + "-srcfiles "+jar+" "
+                    + "-appclass "+main+" "
+                    + "-name "+nApp+" "
+                    + "-title "+title);
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+
+            String line = null;
+
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 }
